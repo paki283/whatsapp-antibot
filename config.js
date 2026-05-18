3df0d985e973ea3559e94f87ed58db066a447a74
paki283 <ladlamalang56@gmail.com>
2026-05-18 14:46:57 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    ownerName: "SILENT Hackers",
+    ownerNumber: "923XXXXXXXXX", // Apna WhatsApp number lagayein country code ke sath
+    prefix: "#",
+    mode: "private", // public ya private
+    viewOnceEmoji: "😭", // Isko aap jab chahein change kar sakte hain
+    
+    // Status Toggles
+    antiDelete: true,
+    antiEdit: true,
+    antiViewOnce: true
+};
